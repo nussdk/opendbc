f3be0b38e1d24c9fdcb1c0738cfb62e1b7ea4185
nussdk <nussdorfer.k@gmail.com>
2025-11-15 21:12:31 -0500

    add 2026 carnival

--- opendbc/car/hyundai/fingerprints.py
+++ opendbc/car/hyundai/fingerprints.py
@@ -1189,6 +1189,7 @@ FW_VERSIONS = {
     (Ecu.fwdCamera, 0x7c4, None): [
       b'\xf1\x00KA4HMFC  AT USA LHD 1.00 1.05 99210-R0500 240305',
       b'\xf1\x00KA4HMFC  AT KOR LHD 1.00 1.00 99210-R0600 240924',
+      b'\xf1\x00KA4HMFC  AT USA LHD 1.00 1.00 99210-R0700 250324',
     ],
     (Ecu.fwdRadar, 0x7d0, None): [
       b'\xf1\x00KAhe RDR -----      1.00 1.01 99110-ES500         ',
